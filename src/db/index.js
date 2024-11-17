39659de7fc3aecaa1f3bddffa8ea938b3ad46741
Sowmya K S <119122284+Sowmya-K-S@users.noreply.github.com>
2024-11-17 07:58:56 +0530

    Add code of middlewares and DB Connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,1 +1,18 @@
- 
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connection = async ( ) => {
+    try
+    {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGOOSE_URL}/${DB_NAME}`);
+
+        console.log(`\n MongoDB Connected! \n DB host: ${connectionInstance.connection.host}`);
+    }
+    catch(error)
+    {
+        console.log("MongoDB Connection Could not be done \n\n", error);
+        process.exit(1);
+    }
+}
+
+export { connection }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
- console.log("Its Working yo !!");
+
+import { app } from "./app.js"
+import dotenv from "dotenv"
+import { connection } from "./db/index.js"
+dotenv.config({
+    path:"./src/.env"
+})
+
+const PORT = process.env.PORT || 7001
+
+connection()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`app is listening at port ${PORT}`)
+    })
+    
+})
+.catch((err) => {
+    console.log("MongoDB Connection error: ",err)
+})
+
+
