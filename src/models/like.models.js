cadf70d7e129f60d46c199c6e754e922a886b3b7
Sowmya K S <119122284+Sowmya-K-S@users.noreply.github.com>
2024-11-27 21:15:32 +0530

    add all models

--- src/models/like.models.js
+++ src/models/like.models.js
@@ -1,1 +1,33 @@
- 
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+    {
+        //like can belong to either video, tweet or comment
+
+        video:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video",
+        },
+        tweet:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Tweet",
+        },
+        comment:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+        likedBy:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+
+    }
+)
+
+export const Like = mongoose.model("Like",likeSchema)
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,1 +1,63 @@
- 
+import mongoose, { Schema } from "mongoose";
+
+//"mongoose-aggregate-paginate" :
+//mongoose-aggregate-paginate to run aggregation pipeline queries on database
+// plugin is used as aggregation pipelines were introduced very late and are not directly included in mongoDB
+//so, now they act as plugin and help in writing longer aggregaqtion pipeline queries
+// importing the package
+
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile:
+        {
+            type: String, //cloudinary URL
+            required: true,   
+        },
+        thumbnail:
+        {
+            type: String, //cloudinary URL
+            required: true,
+        },
+        title:
+        {
+            type: String,
+            required: true,
+        },
+        description:
+        {
+            type: String,
+            required: true,
+        },
+        duration:
+        {
+            type: Number,
+            required: true,
+        },
+        views:
+        {
+            type: Number,
+            default: 0,
+        },
+        isPublished:
+        {
+            type: Boolean,
+            default: true,
+        },
+        owner: 
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+//including the mongoose-aggregate-paginate to schema
+videoSchema.plugin(mongooseAggregatePaginate)
+
+//exporting schema
+export const Video = mongoose.model("Video", videoSchema) 
