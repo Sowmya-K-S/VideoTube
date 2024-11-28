9fa834a18c851de75d1c3296155c5761f9780798
Sowmya K S <119122284+Sowmya-K-S@users.noreply.github.com>
2024-11-28 08:02:36 +0530

    add a password encrypt(prehook), password checking, access and refresh token code

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema(
     {
@@ -53,6 +55,52 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 )
+
+//to encrypt the password before saving it to database
+// using prehook
+userSchema.pre("save",async function (next)
+{   
+    if(this.modified("password")) return next() 
+
+    this.password = bcrypt.hash(this.password, 10)
+
+    next()
+})
+
+
+//writing our own middleware
+
+//to check if password in correct or not
+userSchema.methods.isPasswordCorrect = async function(password){
+// here bcrypt will encrypt the incoming password and compare it with encrypted password in database.
+ return await bcrypt.compare(password, this.password) 
+}
+
+//to generate access token
+userSchema.methods.generateAccessToken = function(){
+    //short lived access token (JWT)
+    return jwt.sign({
+        id_ : this._id,
+        email : this.email,
+        username : this.username,
+        fullname : this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    );
+}
+
+
+//to generate refresh token
+userSchema.methods.generateRefreshToken = function(){
+    //short lived access token (JWT)
+    return jwt.sign({
+        id_ : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    );
+}
 
 //to create a document in mongoose
 export const User = mongoose.model("User", userSchema)
